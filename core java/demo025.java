fe6a4be46434eb664202c7a1f128dac5c6edcee4
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-22 22:09:17 +0530

    reverse string

---
+++ core java/demo025.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo025 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
+}
