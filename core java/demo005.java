c3657830fd4460b8da79b42b6095ddb8c2c04692
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-22 21:55:26 +0530

    while

---
+++ core java/demo005.java
@@ -0,0 +1,10 @@
+class demo005 {
+    public static void main(String args[]){
+        String username = "sai";
+        String pwd ="sai_123";
+        while((username =="sai")&&(pwd=="sai_123")){
+            System.out.println("hello sai");
+            break;
+        }
+    }
+}
