b13983a1c40f6f4acd9916097e8e460914fc7cf5
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-22 21:56:11 +0530

    while

---
+++ core java/demo006.java
@@ -0,0 +1,10 @@
+class demo006{
+    public static void main(String args[]){
+        String username = "sai";
+        String pwd ="sai_123";
+        while((username =="sai")&&(pwd=="sai_123")){
+            System.out.println("Hello Sai welcome to Malla Reddy university");
+            break;
+        }
+    }
+}
