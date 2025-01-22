e6e873c3e25fb3880602e557e3a9b08562ae298a
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-22 22:02:01 +0530

    palindrome

---
+++ core java/demo016.java
@@ -0,0 +1,13 @@
+public class demo016 {
+	public static void main(String[] args) {
+
+				String input = "name";
+				String reversed = new StringBuilder(input).reverse().toString();
+		
+				if (input.equals(reversed)) {
+					System.out.println("The string \"" + input + "\" is a palindrome.");
+				} else {
+					System.out.println("The string \"" + input + "\" is not a palindrome.");
+				}
+			}
+		}
