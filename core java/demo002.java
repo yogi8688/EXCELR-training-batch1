71e311fcee54718dd26634b2cff8942233c24893
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-22 21:52:20 +0530

    simple if&else

---
+++ core java/demo002.java
@@ -0,0 +1,12 @@
+public class demo002{
+    public static void main(String args[]){
+        int num1,num2;
+        num1 = 10;
+        num2 = 20;
+        if(num1 > num2){
+            System.out.println("num1 is greater");
+        }else{
+            System.out.println("num2 is greater");
+        }
+    }
+}
