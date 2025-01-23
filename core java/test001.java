040bf497b096b7327e2eccf7ce63ddeecb369af8
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-23 18:36:51 +0530

    skipping numbers

---
+++ core java/test001.java
@@ -0,0 +1,10 @@
+public class test001{
+        public static void main(String[] args) {
+            for (int i = 1; i <= 10; i++) {
+                if (i == 5||i==7) {
+                    continue;
+                }
+                System.out.println(i);
+            }
+        }
+    }
