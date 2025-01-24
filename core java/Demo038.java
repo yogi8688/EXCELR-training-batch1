d6e25398996676d11de675408bb4caa77d99431c
yogi8688 <maddulayoganandhu@gmail.com>
2025-01-24 18:43:32 +0530

    lowercase upper case

---
+++ core java/Demo038.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Demo038 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a lowercase character: ");
+        char lowerChar = sc.next().charAt(0);
+
+        if(Character.isLowerCase(lowerChar)){
+            char upperChar = Character.toUpperCase(lowerChar);
+            System.out.println("Uppercase character: "+upperChar);
+        }else{
+            System.out.println("Entered character is not in lowercase!");
+        }
+        sc.close();
+
+    }
+}
