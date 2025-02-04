7378581354cb58387261d4f936061252f2bdb6be
yogi8688 <maddulayoganandhu@gmail.com>
2025-02-04 11:31:57 +0530

    uppercase or lowerchar

--- core java/Demo038.java
+++ core java/Demo038.java
@@ -1,17 +1,20 @@
+package Core_java;
+
 import java.util.Scanner;
+
 public class Demo038 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a lowercase character: ");
         char lowerChar = sc.next().charAt(0);
 
-        if(Character.isLowerCase(lowerChar)){
+        if (Character.isLowerCase(lowerChar)) {
             char upperChar = Character.toUpperCase(lowerChar);
-            System.out.println("Uppercase character: "+upperChar);
-        }else{
+            System.out.println("Uppercase character: " + upperChar);
+        } else {
             System.out.println("Entered character is not in lowercase!");
         }
         sc.close();
-
     }
+
 }
