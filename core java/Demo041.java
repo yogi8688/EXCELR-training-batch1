756d5f85891795a2c6216abdf02341bba8ac4cbd
yogi8688 <maddulayoganandhu@gmail.com>
2025-02-04 11:34:28 +0530

    vowel or consonant

---
+++ core java/Demo041.java
@@ -0,0 +1,19 @@
+package Core_java;
+
+import java.util.Scanner;
+
+public class Demo041 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a Character:");
+            char c = sc.next().charAt(0);
+
+            if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')) {
+                System.out.println(c + " is a vowel");
+            } else {
+                System.out.println(c + " is a consonant");
+            }
+        }
+
+    }
+}
