1605b5678a23c654bc493048fd61a8dad2f5be8a
yogi8688 <maddulayoganandhu@gmail.com>
2025-02-05 11:16:28 +0530

    sum of digits

---
+++ core java/Demo048.java
@@ -0,0 +1,11 @@
+package Core_java;
+
+import java.util.Scanner;
+
+public class Demo048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+    }
+}
