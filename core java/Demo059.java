416f6c2907906654721711e585c02090b4d61d71
yogi8688 <maddulayoganandhu@gmail.com>
2025-02-07 11:03:36 +0530

    Prime or not

---
+++ core java/Demo059.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Demo059 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        if(isPrime(num)){
+            System.out.println(num + " is a prime number");
+        }
+        else{
+            System.out.println(num + " is not a prime number");
+        }
+        sc.close();
+    }
+   
+    public static boolean isPrime(int num){
+        if(num <= 1){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
