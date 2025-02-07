343644a04e5a6948ab628e1faf1cce871e923b0a
yogi8688 <maddulayoganandhu@gmail.com>
2025-02-07 10:53:44 +0530

    5 table

---
+++ core java/Demo0050.java
@@ -0,0 +1,8 @@
+public class Demo0050 {
+    public static void main(String[] args){
+        int number=5;
+        for(int i=1;i<=10;i++){
+            System.out.println(number +" x "+i+" = "+number*i);
+        }
+    }
+}
